ee1b4f6a96986c30741207c864aa3ac0261a869b
MOHAMMAD NAWAID KALIM <nawaidkalim24080@gmail.com>
2026-09-05 17:29:41 +0400

    Use FlatCompat for Next 15 ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   globalIgnores([
     ".next/**",
     "out/**",
