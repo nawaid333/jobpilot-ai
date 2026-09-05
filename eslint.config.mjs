2f103b2e2d763060bfe62a1fa0014f2ab1083ff3
MOHAMMAD NAWAID KALIM <nawaidkalim24080@gmail.com>
2026-09-05 17:23:27 +0400

    Fix Next.js ESLint flat config composition

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  nextVitals,
-  nextTs,
+  ...nextVitals,
+  ...nextTs,
   globalIgnores([
     ".next/**",
     "out/**",
