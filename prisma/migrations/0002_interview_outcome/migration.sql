3c16eb5cda0b44d5aaf4016d0d25214314ce599e
MOHAMMAD NAWAID KALIM <nawaidkalim24080@gmail.com>
2026-09-10 08:26:27 +0400

    Add interview outcome migration

---
+++ prisma/migrations/0002_interview_outcome/migration.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "Application" ADD COLUMN "interviewCompletedAt" TIMESTAMP(3);
+ALTER TABLE "Application" ADD COLUMN "interviewOutcome" TEXT;
+ALTER TABLE "Application" ADD COLUMN "followUpDueAt" TIMESTAMP(3);
