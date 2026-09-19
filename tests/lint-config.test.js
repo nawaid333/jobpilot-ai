56cc07d5cf28c25f78629edf75aa71d5016dbc5c
MOHAMMAD NAWAID KALIM <nawaidkalim24080@gmail.com>
2026-09-19 17:00:49 +0400

    test: guard against JSX apostrophe lint regressions

--- tests/lint-config.test.js
+++ tests/lint-config.test.js
@@ -20,3 +20,20 @@ test("lint command is non-interactive and uses a flat-compatible Next.js config"
   assert.match(config, /\.next\/\*\*/);
   assert.match(config, /node_modules\/\*\*/);
 });
+
+test("linted pages escape apostrophes in JSX text", async () => {
+  const pages = [
+    ["automation", "employer's", "employer&apos;s"],
+    ["dashboard", "Don't guess", "Don&apos;t guess"],
+    ["jobs", "Don't just find jobs", "Don&apos;t just find jobs"],
+    ["profile", "what you're looking for", "what you&apos;re looking for"],
+    ["tailor", "You'll get", "You&apos;ll get"],
+    ["tailor", "employer's application", "employer&apos;s application"],
+  ];
+
+  for (const [page, rawText, escapedText] of pages) {
+    const content = await readFile(new URL(`../src/app/${page}/page.tsx`, import.meta.url), "utf8");
+    assert.doesNotMatch(content, new RegExp(`<[^>]+>[^<]*${rawText.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}`));
+    assert.match(content, new RegExp(escapedText.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")));
+  }
+});
